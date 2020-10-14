6a4e54253084c811a2a4eee23a7bfaf44aa75b42
shweta6674 <70576647+shweta6674@users.noreply.github.com>
2020-10-14 18:55:02 +0530

    Update myjss.js

--- myjss.js
+++ myjss.js
@@ -1,13 +1,16 @@
+//js file for js properties in jquery
 
 
 $(document).ready(function(){
- 
+
+/*blinking of my logo on naviagtion bar*/
 function blink_text(){
 	$('.logo').fadeOut(100);
 	$('.logo').fadeIn(100);
 }
 setInterval(blink_text,1000);
-			   
+		
+/*Seeing the testimonial on hover on the text*/		
 $(".journey").mouseenter(function() {
     $(".screen").stop().show();
 });
